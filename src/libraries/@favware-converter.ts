f0d07a38c4a094fc6df7834516d2f2e6be320b99
Jonah Snider <jonah@jonah.pw>
2020-12-24 06:49:30 -0800

    docs(@favware-converter): fix bug comment

--- src/libraries/@favware-converter.ts
+++ src/libraries/@favware-converter.ts
@@ -5,7 +5,7 @@ export const name = '@favware/converter';
 
 export default {
 	[BenchmarkTitles.InchesToFeet]() {
-		// There is a bug in @favware/converter that is swapping the from <-> type, but only for length which is why this is `'ft', 'in'` instead of `'in', 'ft'`
+		// There is a bug in @favware/converter that is swapping the from <-> to units, but only for length which is why this is `'ft', 'in'` instead of `'in', 'ft'`
 		return convert(1024, 'ft', 'in');
 	},
 	[BenchmarkTitles.FractionLitersToCubicInches]() {
