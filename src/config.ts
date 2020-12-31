1acaf762b404f786f7be015cf87c770bae2299c1
Jonah Snider <jonah@jonah.pw>
2020-12-30 18:00:09 -0800

    fix: revert change to number of trials per benchmark

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 /** Total trials to use in results. */
-export const trials = 100;
+export const trials = 100_000;
 
 /** Number of trials to perform as warmup period, these are discarded. */
 export const skip = 500;
