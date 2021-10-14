03379a72c5554211002e98839a518162a01e9059
Jonah Snider <jonah.snider@voiceflow.com>
2021-10-13 17:52:34 -0700

    feat(simple-units): add simple-units benchmarks (#178)

---
+++ src/libraries/simple-units.ts
@@ -0,0 +1,23 @@
+import simpleUnits from 'simple-units';
+import {BenchmarkTitles, LibraryCategory} from '../config.js';
+
+// simple-units typings are incorrect
+const units = (simpleUnits as any).default as typeof simpleUnits;
+
+export const name = 'simple-units';
+
+export const category = LibraryCategory.Fast;
+
+const tests = {
+	[BenchmarkTitles.InchesToMillimeters]() {
+		return units.from(4, 'inch').to('millimeters');
+	},
+	[BenchmarkTitles.LitersToCubicInches]() {
+		return units.from(2.5, 'l').to('in3');
+	},
+	[BenchmarkTitles.HoursToMinutes]() {
+		return units.from(24, 'hours').to('minutes');
+	},
+};
+
+export default tests;
